625b07314e6b6c0cb4360e0715ad52810072bff9
Git-it-Samer <samerkhalil1993@gmail.com>
2020-03-25 18:10:39 -0700

    Update docstrings on bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -17,33 +17,33 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid         inputs
-    # Applying this filter will run repeatedly until user input is recognized. 
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # Applying this filter will run repeatedly until user input is recognized.
     while True:
-        # Applying the lower method below will create a standardization for user input and if the first letter is               capitalized, it will be recognized either way.
+        # Applying the lower method below will create a standardization for user input and if the first letter is capitalized, it will be recognized either way.
         city = input('\nWould you like to explore data for Chicago, New York City or Washington?\n')
         if city.lower() not in CITY_DATA.keys():
             print('Input\’s invalid, please select one of the three cities provided')
         else:
             city = city.lower()
-            # Breaking the loop will carry this statement over to the proceeding statements. 
+            # Breaking the loop will carry this statement over to the proceeding statements.
             break
-    # Used title method to capitalize the first letter of user's answers for consistency, especially since city names       are involved. 
+    # Used title method to capitalize the first letter of user's answers for consistency, especially since city names are involved.
     print('You chose {} '.format(city.title()))
 
     # TO DO: get user input for month (all, january, february, ... , june)
     # A dictionary is made to simplify accessing month data, including the option of 'all'.
-    MONTH_DATA = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'all': 7} 
+    MONTH_DATA = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'all': 7}
     while True:
         month = input('\nWhich month(s), between January and June, would you like to see data for? If you want all months, please type ‘all’.\n')
         if month.lower() not in MONTH_DATA.keys():
             print('Input\’s invalid, please select a month between January and June or ‘all’.')
         else:
             month = month.lower()
-            break    
-     
-    print('You chose {} '.format(month.title()))            
-    
+            break
+
+    print('You chose {} '.format(month.title()))
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     # A dictionary was also made for the days of the week, enabling another source to filter.
     DAY_DATA = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4, 'saturday': 5, 'sunday': 6,  'all': 7}
@@ -54,7 +54,7 @@ def get_filters():
         else:
             day = day.lower()
             break
-    
+
     print('You chose {} '.format(month.title()))
 
     print('-'*40)
@@ -81,14 +81,14 @@ def load_data(city, month, day):
     df['day_of_week'] = df['Start Time'].dt.weekday_name
     # An if loop was used for filtering months and day.
     if month != 'all':
-        # An index for the months was created as well as adding 1 to access corresponding integer, since the first             element is recognized as 0 in Python.
+        # An index for the months was created as well as adding 1 to access corresponding integer, since the first element is recognized as 0 in Python.
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
         #New dataframe enables filtering by month.
         df = df[df['month'] == month]
- 
+
     if day != 'all':
-        # New dataframe enables filtering by weekdays. 
+        # New dataframe enables filtering by weekdays.
         df = df[df['day_of_week'] == day.title()]
 
     # The files chosen will be called as a statement and returned as a dataframe.
@@ -96,8 +96,12 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """
+    Displays statistics on the most frequent times of travel.
 
+    Args:
+        df: The dataframe of your choosing.
+    """
     print('\nCalculating The Most Frequent Times of Travel...\n')
     # Time method was used to convert time into seconds.
     start_time = time.time()
@@ -123,8 +127,12 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    """
+    Displays statistics on the most popular stations and trip.
 
+    Args:
+        df: The dataframe of your choosing.
+    """
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
@@ -141,23 +149,27 @@ def station_stats(df):
     # TO DO: display most frequent combination of start station and end station trip
     # Combined the 'Start Station' and 'End Station' columns in the DataFrame by using str.cat
     df['Start To End'] = df['Start Station'].str.cat(df['End Station'], sep=', ')
-    # The results will transfer into a new column, 'Start To End'. Mode was used as well to find the most frequent         result.
+    # The results will transfer into a new column, 'Start To End'. Mode was used as well to find the most frequent result.
     common_combination = df['Start To End'].mode()[0]
     print('The most frequent combination of start and end station trip: {}'.format(common_combination))
-   
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """
+    Displays statistics on the total and average trip duration.
 
+    Args:
+        df: The dataframe of your choosing.
+    """
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
     # TO DO: display total travel time
-    # The total travel time is calculated with the sum method. 
+    # The total travel time is calculated with the sum method.
     total_duration = df['Trip Duration'].sum()
     print('Total travel time: {}'.format(total_duration))
 
@@ -171,8 +183,12 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """
+    Displays statistics on bikeshare users.
 
+    Args:
+        df: The dataframe of your choosing.
+    """
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
@@ -182,12 +198,12 @@ def user_stats(df):
     print('User Types\n', user_types)
 
     # TO DO: Display counts of gender
-    # Used the if loop, since the 'Gender' column doesn't exist in the washington.csv file. 
+    # Used the if loop, since the 'Gender' column doesn't exist in the washington.csv file.
     if 'Gender' in df.columns:
         # Genders are categorized and then quantified with the value_counts method.
         gender_counts = df['Gender'].value_counts()
         print('\nThe counts of each gender type:\n', gender_counts)
-        
+
     else:
         print('\nThere are no columns for the value in the file.')
 
@@ -201,26 +217,30 @@ def user_stats(df):
         print('\nThe earliest year of birth: {}'.format(earliest_year))
         print('The most recent year of birth: {}'.format(recent_year))
         print('The most common year of birth: {}'.format(common_year))
-        
+
     else:
         print('\nThere are no columns for the value in the file.')
-       
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-    
+
 
 def display_raw_data(df):
-    """Displays 5 rows of raw data for the csv file for the selected city. User has choice to view, as well as an           additional 5 rows until no longer desired"""
-    
+    """
+    Displays 5 rows of raw data for the csv file for the selected city. User has choice to view, as well as an additional 5 rows until no longer desired.
+
+    Args:
+        df: The dataframe of your choosing.
+    """
     row_length = df.shape[0]
 
     # iterate from 0 to the number of rows in intervals of 5
     for i in range(0, row_length, 5):
-        
+
         yes = input('\nDo you want to see 5 lines of the raw data? Type \'yes\' or \'no\'\n> ')
         if yes.lower() != 'yes':
             break
-        
+
         # Retrieved and converted data to json format and spliting each json row of data with the split method.
         # Used 'W3resource' as guidance in using Pandas DataFrame with json
         raw_data = df.iloc[i: i + 5].to_json(orient='records', lines=True).split('\n')
@@ -230,7 +250,7 @@ def display_raw_data(df):
             json_row = json.dumps(parsed_row, indent=2)
             print(json_row)
 
-    
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -249,4 +269,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
